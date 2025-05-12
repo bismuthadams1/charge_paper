a7cfddee777b29bc9d6056d580608309111c895c
Charlie Adams <bismuthadams@gmail.com>
2025-05-12 16:07:52 +0100

    recreate esp

--- benchmark_wf_analysis/make_esp_db/produce_db.py
+++ benchmark_wf_analysis/make_esp_db/produce_db.py
@@ -8,7 +8,7 @@ from chargecraft.storage.qcarchive_transfer import QCArchiveToLocalDB
 from openff.recharge.grids import LatticeGridSettings
 from chargecraft.storage.storage import MoleculePropRecord, MoleculePropStore
 
-client = PortalClient("")  #add address of local qcarchive instance
+client = PortalClient("http://127.0.0.1:7777")  #add address of local qcarchive instance
 
 # Define the grid that the electrostatic properties will be trained on and the
 # level of theory to compute the properties at.
